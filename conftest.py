00d8002e5f423f1381e2419887e35f31b44d6835
Timofey <timayakovishin@gmail.com>
2023-01-03 17:25:15 +0300

    updated fiels

--- conftest.py
+++ conftest.py
@@ -1,28 +1,32 @@
-import webbrowser
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.options import Options as OptionFirefox
 
+
 def pytest_addoption(parser):
+    # command for identify the language
     parser.addoption('--language', action='store', default="es",
                      help="Choose browser: es or ru")
+    # command for identify the browser
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
+    """main function. except commands for choose specific language and browser.
+    It launches browsers and closes them"""
     browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
     browser = None
     if browser_name == "chrome":
         options = Options()
-        options.add_experimental_option('prefs', {"intl.accept_languages": user_language})
+        # options.add_experimental_option('prefs', {"intl.accept_languages": user_language})
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         option_firefox = OptionFirefox()
-        option_firefox.set_preference("intl.accept_languages", user_language)
+        # option_firefox.set_preference("intl.accept_languages", user_language)
         browser = webdriver.Firefox(options=option_firefox)
     yield browser
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -3,8 +3,13 @@ import time
 from selenium.webdriver.common.by import By
 
 
-
 def test_find_the_button(browser, request):
-    browser.get(f"https://selenium1py.pythonanywhere.com/{request.config.getoption('language')}/catalogue/coders-at-work_207/")
-    button = browser.find_element(By.CSS_SELECTOR, "#add_to_basket_form button")
-    time.sleep(1)
+    lang_of_counrty = request.config.getoption('language')
+    browser.get(f"https://selenium1py.pythonanywhere.com/{lang_of_counrty}/catalogue/coders-at-work_207/")
+    time.sleep(30)
+    button = browser.find_element(By.CSS_SELECTOR, '#add_to_basket_form button[type="submit"]')
+    if lang_of_counrty == 'fr':
+        assert button.text == "Ajouter au panier", f"The current language is {lang_of_counrty}, " \
+                                               f"and button name is {button.text}"
+    else:
+        print("В задание описан только вызов для французского языка")
